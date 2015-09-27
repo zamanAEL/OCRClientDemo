d117c68e6b631b507f977a6e1364d4d1252ea548
zamanAEL <assad.zaman@gmail.com>
2015-09-28 05:21:39 +0600

    Create client

--- app/src/main/java/co/celloscope/ocrclientdemo/Client.java
+++ app/src/main/java/co/celloscope/ocrclientdemo/Client.java
@@ -4,34 +4,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class Client extends AppCompatActivity {
+
+    TextView mCallbackText;
+    private OCRClient mOcrClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+        mCallbackText = (TextView) findViewById(R.id.callbackTextView);
+        mOcrClient = new OCRClient(this, mCallbackText);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_client, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_bind:
+                mOcrClient.doBindService();
+                return true;
+            case R.id.action_unbind:
+                mOcrClient.doUnbindService();
+                return true;
+            case R.id.action_ocr:
+                mOcrClient.doOcr();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/co/celloscope/ocrclientdemo/OCRClient.java
@@ -0,0 +1,118 @@
+package co.celloscope.ocrclientdemo;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.annotation.NonNull;
+import android.widget.TextView;
+
+
+public class OCRClient {
+
+    static final int MSG_REGISTER_CLIENT = 1;
+    static final int MSG_UNREGISTER_CLIENT = 2;
+    static final int MSG_SET_VALUE = 3;
+
+    final Messenger mClient = new Messenger(new IncomingHandler());
+    private final ContextWrapper mContextWrapper;
+
+
+    Messenger mService = null;
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mService = new Messenger(service);
+
+            try {
+                Message msg = Message.obtain(null, MSG_REGISTER_CLIENT);
+                msg.replyTo = mClient;
+                mService.send(msg);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            mCallbackText.setText("Service connected and register request sent.");
+
+        }
+
+
+        public void onServiceDisconnected(ComponentName className) {
+            mService = null;
+            mCallbackText.setText("Disconnected from service.");
+        }
+    };
+    boolean mIsBound;
+    final TextView mCallbackText;
+
+    public OCRClient(ContextWrapper mContextWrapper, TextView mCallbackText) {
+
+        this.mContextWrapper = mContextWrapper;
+
+        this.mCallbackText = mCallbackText;
+    }
+
+    void doOcr() {
+        try {
+            if (mService != null) {
+                Message msg = Message.obtain(null,
+                        MSG_SET_VALUE, this.hashCode(), 0);
+                mService.send(msg);
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            e.printStackTrace();
+        }
+    }
+
+    void doBindService() {
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("co.celloscope.ocrservice", "co.celloscope.ocrservice.OCRService"));
+        mContextWrapper.bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            if (mService != null) {
+                try {
+                    Message msg = Message.obtain(null,
+                            MSG_UNREGISTER_CLIENT);
+                    msg.replyTo = mClient;
+                    mService.send(msg);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            mContextWrapper.unbindService(mConnection);
+            mIsBound = false;
+        }
+    }
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void dispatchMessage(@NonNull Message msg) {
+            super.dispatchMessage(msg);
+
+
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SET_VALUE:
+                    mCallbackText.setText("Received from service: " + msg.arg1);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+}
